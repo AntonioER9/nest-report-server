bb6e0e1c255a75e5f5bdf73972f9c76f1692363b
AntonioER9 <aespinozareyes9@gmail.com>
2024-08-02 16:04:58 -0400

    add axios and kurkle colors

--- src/store-reports/store-reports.controller.ts
+++ src/store-reports/store-reports.controller.ts
@@ -4,7 +4,7 @@ import { Response } from 'express';
 
 @Controller('store-reports')
 export class StoreReportsController {
-  constructor(private readonly storeReportsService: StoreReportsService) {}
+  constructor(private readonly storeReportsService: StoreReportsService) { }
 
   @Get('orders/:orderId')
   async getOrderReport(
@@ -15,6 +15,26 @@ export class StoreReportsController {
 
     response.setHeader('Content-Type', 'application/pdf');
     pdfDoc.info.Title = 'Order-Report';
+    pdfDoc.pipe(response);
+    pdfDoc.end();
+  }
+
+  @Get('svgs-charts')
+  async getSvgChart(@Res() response: Response) {
+    const pdfDoc = await this.storeReportsService.getSvgChart();
+
+    response.setHeader('Content-Type', 'application/pdf');
+    pdfDoc.info.Title = 'Svg-Chart-Report';
+    pdfDoc.pipe(response);
+    pdfDoc.end();
+  }
+
+  @Get('statistics')
+  async statistics(@Res() response: Response) {
+    const pdfDoc = await this.storeReportsService.getStatistics();
+
+    response.setHeader('Content-Type', 'application/pdf');
+    pdfDoc.info.Title = 'Statistics-Report';
     pdfDoc.pipe(response);
     pdfDoc.end();
   }
